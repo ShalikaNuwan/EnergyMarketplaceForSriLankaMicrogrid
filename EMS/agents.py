a5937c6b55512829f8397a3df48ec6b426dd3a5f
ShalikaNuwan <kapilarathnepgsn.20@uom.lk>
2025-05-14 23:10:28 +0530

    created the generator Agent inheriting from the ems Agent

--- EMS/agents.py
+++ EMS/agents.py
@@ -2,7 +2,6 @@ from pydantic import BaseModel
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.prompts import PromptTemplate
 from typing import List
-import json
 
 class HourlyMatch(BaseModel):
     time : str
@@ -24,7 +23,7 @@ class EMSMatchProfile(BaseModel):
 class EMSAgent:
     def __init__(self,model):
         self.parser = PydanticOutputParser(pydantic_object=EMSMatchProfile)
-        self.ems_system_prompt = PromptTemplate(
+        self.system_prompt = PromptTemplate(
         template=(
             '''
             You are a Energy expert who is capable of matching the demand and supply of microgrid. The task is to match the demand and supply for the microgrid. 
@@ -58,9 +57,42 @@ class EMSAgent:
                         
     def execute(self,solar_arr,demand_arr,battery_soc):
         query = self.input_query.format(solar_arr,demand_arr,battery_soc)
-        formatted_prompt = self.ems_system_prompt.format(input_data=query)
+        formatted_prompt = self.system_prompt.format(input_data=query)
         response = self.model.invoke(formatted_prompt)
         structured_output = self.parser.parse(response.content)
         json_data = [record.dict() for record in structured_output.matched_results]
         return json_data
         
+
+class GeneratorAgenet(EMSAgent):
+    def __init__(self, model):
+        super().__init__(model)
+        self.system_prompt = PromptTemplate(
+        template=(
+            '''
+            You are an expert of generators used to supply electricity in the microgrid. 
+            You will be given a json file which containes the next 24 hours hourly matched energy profiles.
+            What you need to do is, check whether the generator need to be used in that hour.If the generator is used, the generator_used is set to True.
+            Then get the energy needed from the generator from the variable demand_shortage_amount.
+            Considering the energy needed from the generator, you need to decide in which capacity should the generator run.
+            Here are the specifications of the generator.
+            Generator rated capacity: 50kWh.
+            Running levels: 0.25, 0.5, 0.75, 1
+            
+            the excess energy generated from the generator should store to the battery bank and update the soc level.
+            Finally output the corrected json file considering these facts.
+            '''
+            "Return the matched data in the following format:\n{format_instructions}\n\n"
+            "Here is the input data:\n{input_data}"
+        ),
+        input_variables=["input_data"],
+        partial_variables={"format_instructions": self.parser.get_format_instructions()}
+        
+        )
+        
+    def execute(self, json_file):
+        self.system_prompt = self.system_prompt.format(input_data= json_file)
+        response = self.model.invoke(self.system_prompt)
+        structured_output = self.parser.parse(response.content)
+        json_data = [record.dict() for record in structured_output.matched_results]
+        return json_data
