22862a7c0f2b3db93d511931e9e8b537e0c71ceb
ShalikaNuwan <kapilarathnepgsn.20@uom.lk>
2025-07-08 18:26:33 +0530

    added the condition_encoder column and month,day, day_of_week,day_of_year column to the dataset

--- ModelRunner/dataPreprocessing.py
+++ ModelRunner/dataPreprocessing.py
@@ -25,7 +25,11 @@ def create_input_rf():
     weatherJson = get_weather_data()
     weather_df = weather_data_to_dataframe(weatherJson)
     weather_encoder = load_label_encoders('WeatherConditions')
-    weather_df['conditions'] = weather_encoder.transform(weather_df['conditions'])
+    weather_df['conditions_encoded'] = weather_encoder.transform(weather_df['conditions'])
+    weather_df['month'] = weather_df.index.month
+    weather_df['day'] = weather_df.index.day
+    weather_df['day_of_week'] = weather_df.index.day_of_week
+    weather_df['day_of_year'] = weather_df.index.day_of_year
     x = weather_df[['temp','humidity','windgust','solarradiation','conditions_encoded','month','day','day_of_week','day_of_year']]
     
     return x
