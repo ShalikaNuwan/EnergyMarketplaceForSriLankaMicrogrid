dcbd6c63cb35cae79b629c590846f1ee1820e52e
ShalikaNuwan <kapilarathnepgsn.20@uom.lk>
2025-07-09 15:47:33 +0530

    added azure function to store the last hour solar geneation into cosmosDB

---
+++ AzureFunctions/DevEnergyMarketSolarActualUpload/function_app.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import logging
+import azure.functions as func
+from dotenv import load_dotenv
+from azure.cosmos import CosmosClient
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+from ModelRunner.getWeatherData import weather_data_to_dataframe,get_weather_data,get_current_time
+
+def generation_of_hour():
+    weather_json = get_weather_data()
+    df,_ = weather_data_to_dataframe(weather_json)
+    _,previous_hour = get_current_time()
+
+    df = df[df.index == previous_hour]
+    generation = round((df['solarradiation']*(257.27/1000)*0.9/6.43).to_list()[0],2)
+    
+    return generation,previous_hour
+
+def fetch_data():
+    load_dotenv()
+    primary_key = os.getenv('COSMOS_DB_KEY')
+    database_url = os.getenv('COSMOS_DB_URL')
+    database_name = os.getenv('DATABASE')
+    container_name = os.getenv('CONTAINER')
+
+    client = CosmosClient(database_url,primary_key)
+    database = client.get_database_client(database_name)
+    container = database.get_container_client(container_name)
+    
+    solar_gen,previous_hour = generation_of_hour()
+    container.upsert_item({
+        'id' : previous_hour,
+        'Location' : 'UoM',
+        'SolarGeneration' : solar_gen
+    })
+    
+app = func.FunctionApp()
+
+@app.timer_trigger(schedule="0 2 * * * *", arg_name="myTimer", run_on_startup=True,
+              use_monitor=False) 
+def DevEnergyMarketSolarActualUpload(myTimer: func.TimerRequest) -> None:
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+    fetch_data()
+
+    logging.info('Python timer trigger function executed.')
